0bc9410a33fb54fcc323385ef419f829c01daa90
luanpedrosa <83519092+luan048@users.noreply.github.com>
2024-05-30 14:36:12 -0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,52 @@
+import {fastify} from 'fastify'
+import {DatabaseMemory} from './database-memory.js'
+import { Console } from 'console'
+import { request } from 'http'
+
+const server = fastify()
+
+const database = new DatabaseMemory()
+
+server.post('/videos', (request, reply) => {
+
+    const {title, description, duration} = request.body
+
+    database.create({
+        title,
+        description,
+        duration,
+    })
+
+    return reply.status(201).send()
+})
+
+server.get('/videos', () => {
+    const videos = database.list()
+
+    return videos
+})
+
+server.put('/videos/:id', (request, reply) => {
+    const videoId = request.params.id
+    const {title, description, duration} = request.body
+
+    database.update(videoId, {
+        title,
+        description,
+        duration
+    })
+
+    return reply.status(204).send()
+})
+
+server.delete('/videos/:id', (request, reply) => {
+    const videoId = request.params.id
+
+    database.delete(videoId)
+
+    return reply.status(204).send()
+})
+
+server.listen({
+    port: 5555
+})
